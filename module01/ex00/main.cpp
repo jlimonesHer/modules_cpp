3fe175cfac3fdd853043c87b692ece0dfaa66b20
jlimones <jlimones@student.42malaga.com>
2023-06-14 12:52:18 +0200

    cpp_01 ex00 finish

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -3,22 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlimones <josec.limones@gmail.com>         +#+  +:+       +#+        */
+/*   By: jlimones <jlimones@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 11:49:05 by jlimones          #+#    #+#             */
-/*   Updated: 2023/06/12 18:05:25 by jlimones         ###   ########.fr       */
+/*   Updated: 2023/06/14 12:50:06 by jlimones         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "zombie.hpp"
 
+void leaks()
+{
+    system("leaks brainz");
+}
+
 int main(void)
 {
+    atexit(leaks);
     Zombie *zombie_ptr;
-
+    
     zombie_ptr = newZombie("Limones");
     zombie_ptr->announce();
     randomChump("Hernandez");
-    
+    delete zombie_ptr;
     return 0;
 }
