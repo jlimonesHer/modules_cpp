8fb5bc025126680cf259b64316cfdf9dac8239eb
jlimones <user@jlimones.local>
2023-06-19 09:23:16 +0200

    cpp01_ex04 finish

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlimones <josec.limones@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 16:34:24 by jlimones          #+#    #+#             */
-/*   Updated: 2023/06/17 09:44:35 by jlimones         ###   ########.fr       */
+/*   Updated: 2023/06/19 09:22:16 by jlimones         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <fstream>
 #include <string>
 
-void replaceAllOccurrences(std::string& content, const std::string& s1, const std::string& s2) {
+void replaceAllOccurrences(std::string &content, const std::string &s1, const std::string &s2) {
     size_t pos = 0;
     while ((pos = content.find(s1, pos)) != std::string::npos) {
         content = content.substr(0, pos) + s2 + content.substr(pos + s1.length());
@@ -26,7 +26,7 @@ void replaceAllOccurrences(std::string& content, const std::string& s1, const st
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv) {
     if (argc != 4) {
         std::cout << "Usage: program <filename> <s1> <s2>" << std::endl;
         return 1;
@@ -35,27 +35,23 @@ int main(int argc, char* argv[]) {
     std::string filename = argv[1];
     std::string s1 = argv[2];
     std::string s2 = argv[3];
-
     std::ifstream inputFile(filename.c_str());
+    std::ofstream outputFile((filename + ".replace").c_str());
+    std::string line;
+    
     if (!inputFile) {
         std::cout << "Failed to open input file." << std::endl;
         return 1;
     }
-
-    std::ofstream outputFile((filename + ".replace").c_str());
     if (!outputFile) {
         std::cout << "Failed to create output file." << std::endl;
         return 1;
     }
-
-    std::string line;
     while (std::getline(inputFile, line)) {
         replaceAllOccurrences(line, s1, s2);
         outputFile << line << std::endl;
     }
-
     std::cout << "Replacement completed. Generated file: " << filename << ".replace" << std::endl;
-
     inputFile.close();
     outputFile.close();
 
