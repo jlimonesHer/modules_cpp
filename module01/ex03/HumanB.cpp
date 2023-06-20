f8d6f3df1423416872630526ca56729763a086cc
jlimones <jlimones@student.42malaga.com>
2023-06-20 12:50:30 +0200

    cpp_01 ex03 solved

--- module01/ex03/HumanB.cpp
+++ module01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: jlimones <jlimones@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:06:22 by jlimones          #+#    #+#             */
-/*   Updated: 2023/06/15 16:14:09 by jlimones         ###   ########.fr       */
+/*   Updated: 2023/06/20 12:48:28 by jlimones         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,20 @@ HumanB::~HumanB()
 }
 
 void HumanB::attack(){
+    std::string weapon;
+
+    if (_weapon == NULL || _weapon->getType() == ""){
+    std::cout << BLUE << _name 
+              << YELLOW << " attacks with their "
+              << RED << "no weapon"
+              << NO_COLOR << std::endl;
+    }
+    else{
     std::cout << BLUE << _name 
               << YELLOW << " attacks with their "
               << RED << _weapon->getType()
               << NO_COLOR << std::endl;
+    }
 }
 
 
