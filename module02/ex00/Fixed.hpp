7c0530717e99a9ab68358a7908b3b105ae6df394
jlimones <jlimones@student.42malaga.com>
2023-06-21 13:39:41 +0200

    cpp_02 ex00 finish

--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,7 +6,28 @@
 /*   By: jlimones <jlimones@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 16:58:15 by jlimones          #+#    #+#             */
-/*   Updated: 2023/06/20 16:58:16 by jlimones         ###   ########.fr       */
+/*   Updated: 2023/06/21 13:09:41 by jlimones         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+    private:
+        int _valueFixed;
+        static const int _fixed = 8;
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(Fixed const &copy);
+        Fixed	&operator=(Fixed const &copy);
+
+        int getRawBits(void)  const;
+        void setRawBits(int const raw);
+};
+
+#endif
