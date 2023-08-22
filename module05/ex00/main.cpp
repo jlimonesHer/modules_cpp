d85fd76b1c65fe11f60ade79101f0e421c2c4c0a
jlimones <jlimones@student.42malaga.com>
2023-08-22 12:23:02 +0200

    module05 finish submited

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlimones <jlimones@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 11:55:56 by jlimones          #+#    #+#             */
-/*   Updated: 2023/08/19 18:24:57 by jlimones         ###   ########.fr       */
+/*   Updated: 2023/08/22 09:58:37 by jlimones         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,29 @@
 
 int main()
 {   
+    Bureaucrat one("one", 148);
+    Bureaucrat three("three", 2);
+    
     try {
-        Bureaucrat one("one", 148);
-        //Bureaucrat two("two", 151);
-        Bureaucrat three("three", 2);
+        Bureaucrat two("two", 151);
+    } catch(std::exception& exception) {
+        std::cout << exception.what() << std::endl;
+    }
         
-        // std::cout << one;
-        // one.decrementGrade();
-        // std::cout << one;
-        // one.decrementGrade();
-        // std::cout << one;
-        // one.decrementGrade();
-        // std::cout << one;
+    try {
+        std::cout << one;
+        one.decrementGrade();
+        std::cout << one;
+        one.decrementGrade();
+        std::cout << one;
+        one.decrementGrade();
+        std::cout << one;
+    }
+     catch(std::exception& exception) {
+        std::cout << exception.what() << std::endl;
+    }
         
+    try {
         std::cout << three;
         three.incrementGrade();
         std::cout << three;
